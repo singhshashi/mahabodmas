a76bba97355bf4b16aa4150c46fdc74ad5c04109
Shashi Singh <singh.shashishekhar@gmail.com>
2018-01-09 13:41:21 +0530

    Added Tweet your score and Google Analytics

--- src/GameOverMessage.js
+++ src/GameOverMessage.js
@@ -18,12 +18,20 @@ class GameOverMessage extends Component {
         ];
 
         return gameOverMessages[Math.floor(Math.random()*gameOverMessages.length)];
-    };
+    }; 
+
+    componentDidUpdate(prevProps, prevState) {
+        if (!prevProps.show && this.props.show) {            
+                window.twttr.widgets.load();            
+        }
+    }
 
     render() {
         if (!this.props.show) {
             return (<div></div>);
         }
+
+        const tweetMessage = "Beat my score of " + this.props.highscore + " in a simple mental math game at"
 
         return (
             <div>
@@ -46,6 +54,20 @@ class GameOverMessage extends Component {
                     </div>
                     <div className="col-6-sm right">
                         <p>Highscore: {this.props.highscore}</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="sol-s12-sm right">
+                        <p>
+                            <a className="twitter-share-button"
+                                href="https://twitter.com/intent/tweet"
+                                data-size="large"
+                                data-text={tweetMessage}
+                                data-via="gettrici"
+                                data-hashtags="mahabodmas"
+                                >                                
+                                Tweet</a>
+                        </p>                        
                     </div>
                 </div>
             </div>
